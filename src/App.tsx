3dcb499614fe949a5173707ea5ddedb9ee22c80f
Beso Kurtsadze <beso.qurcadze@gmail.com>
2024-08-22 20:28:37 +0400

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>My App</div>
+import { Routes, Route } from 'react-router-dom'
+import Home from './screens/home'
+import History from './screens/history'
+import Layout from './components/layout'
+
+const ROUTES = {
+  HOME: '/',
+  HISTORY: '/history'
 }
 
-export default App
+export default function App() {
+  return (
+    <Routes>
+      <Route path={ROUTES.HOME} element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path={ROUTES.HISTORY} element={<History />} />
+      </Route>
+    </Routes>
+  )
+}
